a5fcdc946627e385ede4897a3af5f7c0f038e830
ChristianGreen-OSU <85771790+ChristianGreen-OSU@users.noreply.github.com>
2021-07-01 23:01:06 -0400

    Update src.js

--- src.js
+++ src.js
@@ -1,1 +1,14 @@
+let PLACEHOLDER = '.counter-display';
 
+let counterPlusElem = document.querySelector('.button1');
+let conterDisplayElem = document.querySelector(PLACEHOLDER);
+let count = 0;
+
+counterPlusElem.addEventListener("click",()=>{
+  counter++;
+  updateDisplay();
+});
+
+function updateDisplay() {
+    counterDisplayElem.innerHTML = count;
+}
